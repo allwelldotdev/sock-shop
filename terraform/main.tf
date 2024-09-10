5d5c67d039beedae4e539e996f703099343bed6b
Allwell Agwu-Okoro <allwell@allwell.dev>
2024-09-10 13:32:22 +0100

    update eks cluster IAC deployment

--- terraform/main.tf
+++ terraform/main.tf
@@ -21,7 +21,7 @@ resource "random_string" "suffix" {
 
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "5.12.1"
+  version = "5.8.1"
 
   name = "${var.project_name}-vpc"
 
@@ -53,7 +53,7 @@ module "eks" {
   version = "20.8.5"
 
   cluster_name    = local.cluster_name
-  cluster_version = "1.29"
+  cluster_version = "1.30"
 
   cluster_endpoint_public_access           = true
   enable_cluster_creator_admin_permissions = true
@@ -80,6 +80,8 @@ module "eks" {
       min_size     = var.node_count
       max_size     = var.node_count
       desired_size = var.node_count
+
+      # vpc_security_group_ids = [module.eks_sg.security_group_id]
     }
   }
 
@@ -103,74 +105,4 @@ module "irsa-ebs-csi" {
   provider_url                  = module.eks.oidc_provider
   role_policy_arns              = [data.aws_iam_policy.ebs_csi_policy.arn]
   oidc_fully_qualified_subjects = ["system:serviceaccount:kube-system:ebs-csi-controller-sa"]
-}
-
-# create sg with ingress and egress rules
-module "eks_sg" {
-  source  = "terraform-aws-modules/security-group/aws"
-  version = "5.1.2"
-
-  name        = "${var.project_name}-sg"
-  description = "Security group with self ingress, specific open ports, and all egress traffic"
-  vpc_id      = module.vpc.vpc_id
-
-  # TODO: make this security group more secure
-
-  # rule for internal traffic
-  ingress_with_self = [{
-    rule = "all-all"
-  }]
-
-  # ingress rules for specific ports
-  ingress_with_cidr_blocks = [
-    {
-      from_port   = 22
-      to_port     = 22
-      protocol    = "tcp"
-      description = "SSH access"
-      cidr_blocks = "0.0.0.0/0"
-    },
-    {
-      from_port   = 80
-      to_port     = 80
-      protocol    = "tcp"
-      description = "HTTP access"
-      cidr_blocks = "0.0.0.0/0"
-    },
-    {
-      from_port   = 9411
-      to_port     = 9411
-      protocol    = "tcp"
-      description = "Access to port 9411"
-      cidr_blocks = "0.0.0.0/0"
-    },
-    {
-      from_port   = 30001
-      to_port     = 30001
-      protocol    = "tcp"
-      description = "Access to ports 30001"
-      cidr_blocks = "0.0.0.0/0"
-    },
-    {
-      from_port   = 30002
-      to_port     = 30002
-      protocol    = "tcp"
-      description = "Access to ports 30002"
-      cidr_blocks = "0.0.0.0/0"
-    },
-    {
-      from_port   = 31601
-      to_port     = 31601
-      protocol    = "tcp"
-      description = "Access to port 31601"
-      cidr_blocks = "0.0.0.0/0"
-    }
-  ]
-
-  # egress rule for all outbound traffic
-  egress_rules = ["all-all"]
-
-  tags = merge(var.project_tags, {
-    Name = "${var.project_name}-sg"
-  })
 }
